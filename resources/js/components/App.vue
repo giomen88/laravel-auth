263303991875cfa66d3da8ec60ccc0ae89ddf419
giomen88 <giorgia9288@virgilio.it>
2022-09-26 23:08:07 +0200

    migrations + seed + vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>WORK IN PROGRESS</h1>
+        <!-- <div class="card" v-for="post in posts" :key="post.id">
+          <h2>{{ post.title }}</h2>
+          <span> {{ post.slug }} </span>
+          <img :src="post.image" :alt="post['title']" />
+          <p>{{ post.content }}</p>
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Auth::routes(['register' => false]);
+
+// ADMIN
+
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::resource('posts', 'PostController');    
+
+    Route::get('/{any} ', function () {
+        abort('404');
+    })->where('any', '.*');    
+
 });
 
-Auth::routes();
+// GUESTS
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
